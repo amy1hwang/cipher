22631250d0f5b727921f5b599f68ab658b74a6b3
Amy Hwang and Maxwell Cady <amy@>
2016-10-17 14:55:15 -0700

    add jquery

--- js/scripts.js
+++ js/scripts.js
@@ -8,42 +8,51 @@ var capitalize = function(sentence) {
 alert(capitalize(sentence)); */
 
 
+$(document).ready(function() {
+  var sentence = prompt("Please enter a sentence: ");
 
-var sentence = prompt("Please enter a sentence: ");
+  console.log(sentence);
 
-console.log(sentence);
-
-var capitalLetters = function(sentence) {
-  var capitalizedFirstLast = sentence.charAt(0).toUpperCase() + sentence.slice(-1).toUpperCase();
-  return capitalizedFirstLast;
-};
-
-
-// var capitalizedSentence = capitalLetters(sentence)
-
-var reverseLetters = function(capitalizedSentence) {
-  var reversedCapitals = capitalizedSentence.slice(-1) + capitalizedSentence.charAt(0);
-  return reversedCapitals;
-};
+  var capitalLetters = function(sentence) {
+    var capitalizedFirstLast = sentence.charAt(0).toUpperCase() + sentence.slice(-1).toUpperCase();
+    return capitalizedFirstLast;
+  };
 
 
+  // var capitalizedSentence = capitalLetters(sentence)
 
-var callBoth = function(sentence) {
-  var caps = capitalLetters(sentence);
-  return reverseLetters(caps);
-};
+  var reverseLetters = function(capitalizedSentence) {
+    var reversedCapitals = capitalizedSentence.slice(-1) + capitalizedSentence.charAt(0);
+    return reversedCapitals;
+  };
 
-console.log(callBoth(sentence));
 
-var combineEnd = function(sentence) {
-  return sentence + callBoth(sentence);
-};
 
-console.log(combineEnd(sentence));
+  var callBoth = function(sentence) {
+    var caps = capitalLetters(sentence);
+    return reverseLetters(caps);
+  };
 
-var countNumber = function(sentence) {
-  var halfSentence = sentence.length / 2;
-  var middleLetter = sentence.charAt(halfSentence);
-  return middleLetter + sentence;
-};
-console.log(countNumber(sentence).split("").reverse().join(""));
+  console.log(callBoth(sentence));
+
+  var combineEnd = function(sentence) {
+    return sentence + callBoth(sentence);
+  };
+
+  console.log(combineEnd(sentence));
+
+  var countNumber = function(sentence) {
+    var halfSentence = sentence.length / 2;
+    var middleLetter = sentence.charAt(halfSentence);
+    return middleLetter + sentence;
+  };
+  var reverseCountNumber = countNumber(sentence).split("").reverse().join("");
+
+  $("#elephant").click(function() {
+    alert(sentence);
+  });
+
+  $("#panda").click(function() {
+    alert(reverseCountNumber);
+  });
+});
