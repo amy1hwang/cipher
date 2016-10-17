d2ecbba61a8b847efae5df3ad6fe051427f405b1
Amy Hwang and Maxwell Cady <amy@>
2016-10-17 14:39:01 -0700

    Created functions to reverse letters in string

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,49 @@
-var sentence = prompt("Please enter a sentence: ");
+/* var sentence = prompt("Please enter a sentence: ");
 
 var capitalize = function(sentence) {
-  /*var capitalFirst = sentence.charAt(0).toUpperCase() + sentence.slice(1);*/
   var capitalSentence = sentence.charAt(0).toUpperCase() + sentence.slice(1, -1) + sentence.slice(-1).toUpperCase();
   return capitalSentence;
 };
 
-alert(capitalize(sentence));
+alert(capitalize(sentence)); */
+
+
+
+var sentence = prompt("Please enter a sentence: ");
+
+console.log(sentence);
+
+var capitalLetters = function(sentence) {
+  var capitalizedFirstLast = sentence.charAt(0).toUpperCase() + sentence.slice(-1).toUpperCase();
+  return capitalizedFirstLast;
+};
+
+
+// var capitalizedSentence = capitalLetters(sentence)
+
+var reverseLetters = function(capitalizedSentence) {
+  var reversedCapitals = capitalizedSentence.slice(-1) + capitalizedSentence.charAt(0);
+  return reversedCapitals;
+};
+
+
+
+var callBoth = function(sentence) {
+  var caps = capitalLetters(sentence);
+  return reverseLetters(caps);
+};
+
+console.log(callBoth(sentence));
+
+var combineEnd = function(sentence) {
+  return sentence + callBoth(sentence);
+};
+
+console.log(combineEnd(sentence));
+
+var countNumber = function(sentence) {
+  var halfSentence = sentence.length / 2;
+  var middleLetter = sentence.charAt(halfSentence);
+  return middleLetter + sentence;
+};
+console.log(countNumber(sentence).split("").reverse().join(""));
